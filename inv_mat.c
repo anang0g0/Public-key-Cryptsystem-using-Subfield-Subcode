82e560931bd834dbf628bed684d18d92b902f230
test <test@chagai>
2020-10-30 05:29:39 +0900

    modified

--- inv_mat.c
+++ inv_mat.c
@@ -64,29 +64,6 @@ wait (void)
  }
 
 
-/*
-int mlt(int x, int y){
-
-    if(x==0||y==0)
-        return 0;
-
-  return ((x+y-2)%(N-1))+1;
-}
-
-
-int mltn(int n,int x){
-  int i,j;
-
-  if(n==0)
-    return 1;
-  i=x;
-    for(j=0;j<n-1;j++)
-      i=mlt(i,x);
-
-  return i;
-}
-*/
-
 int Inv(unsigned short b){
   int i;
 
