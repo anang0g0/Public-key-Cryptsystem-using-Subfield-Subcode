8e13f3d22ab3818a906d0a4fe3f4249106d21818
test <test@chagai>
2020-10-29 16:21:26 +0900

    modified

--- chash.c
+++ chash.c
@@ -75,6 +75,28 @@ return;
 
 
 
+int mlt(int x, int y){
+
+    if(x==0||y==0)
+        return 0;
+  
+  return ((x+y-2)%(M-1))+1;
+}
+
+
+int mltn(int n,int x){
+  int i,j;
+
+if(n==0)
+return 1;
+  i=x;
+    for(j=0;j<n-1;j++)
+      i=mlt(i,x);
+
+  return i;
+}
+
+
 void pMAT(MAT A,int k,int n,int a){
   int i,j;
 
