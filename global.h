07a46fa7a737de06f1bbb0c4b176e65e00ee20ae
test <test@chagai>
2020-11-02 19:11:35 +0900

    modified

--- global.h
+++ global.h
@@ -10,10 +10,10 @@
 //を表す。ここではDは符号長にしている。
 #define N 16 // (^^;)?
 #define M 16 //有限体の元の数
-#define K (N/2) //符号の次元
+#define K (8) //符号の次元
 #define DEG (K*2)
 #define T (K/2) //エラーの数
-#define U T/2
+#define U K
 #define E (4) //拡大体のビット数
 #define D (16) //符号長（短縮符号）
 #define F  E*K //2040
